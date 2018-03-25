45427eb76e3f887ffa7e8d18ac459dd7f1c2a963
Dasha <dasharyzhenko@ukr.net>
2018-03-25 19:05:19 +0300

    second commit

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 $(document).ready(function(){
       $('.parallax').parallax();
     });
+	
+function parallax(){
+	var prlx_lyr_1 = document.getElementById('prlx_lyr_1');
+	prlx_lyr_1.style.top = -(window.pageYOffset * 2)+'px';
+	
+}
+window.addEventListener("scroll", parallax, false);
+
+
